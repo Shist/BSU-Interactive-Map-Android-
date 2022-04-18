ed20acfb8de1659bc15473588e9dcf39d4c939cf
Shist <shist.pupust@mail.ru>
2022-04-18 18:44:53 +0300

    Now, application can set markers of buildings on the map!!!

--- ui/src/main/java/com/example/ui/fragments/MapFragment.kt
+++ ui/src/main/java/com/example/ui/fragments/MapFragment.kt
@@ -14,17 +14,31 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.domain.BuildingItem
 import com.example.ui.R
 import com.example.ui.mapbox.LocationPermissionHelper
+import com.example.ui.viewModels.LoadState
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
 import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 import com.mapbox.maps.plugin.gestures.OnMoveListener
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.lang.ref.WeakReference
 
 
@@ -88,6 +102,90 @@ class MapFragment : Fragment(), KoinComponent {
         locationPermissionHelper.checkPermissions {
             onMapReady()
         }
+
+        var dataList: List<BuildingItem> = emptyList()
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.dataFlow.collect {
+                    dataList = it
+                    setMarkers(dataList)
+                }
+            }
+        }
+
+        val timeLength = if (dataList.isEmpty()) // If not data in general (even in database)
+            Snackbar.LENGTH_INDEFINITE
+        else { // If no NEW data, but we have OLD data in database
+            Snackbar.LENGTH_LONG
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collect {
+                    when (it) {
+                        LoadState.LOADING -> {
+                            createSnackbar(resources.getString(R.string.loading),
+                                requireContext().getColor(R.color.black))
+                        }
+                        LoadState.SUCCESS -> {
+                            createSnackbar(resources.getString(R.string.loadingSuccess),
+                                requireContext().getColor(R.color.black))
+                            setMarkers(dataList)
+                        }
+                        LoadState.INTERNET_ERROR -> {
+                            createSnackbarWithReload(timeLength,
+                                resources.getString(R.string.errorNetwork))
+                        }
+                        LoadState.UNKNOWN_ERROR -> {
+                            createSnackbarWithReload(timeLength,
+                                resources.getString(R.string.errorUnknownNoData))
+                        }
+                        LoadState.EMPTY_DATA_ERROR -> {
+                            createSnackbarWithReload(timeLength,
+                                resources.getString(R.string.errorNoNewsOnAPI))
+                        }
+                        else -> createSnackbar(resources.getString(R.string.launching),
+                            requireContext().getColor(R.color.black))
+                    }
+                }
+            }
+        }
+
+        if (savedInstanceState == null && viewModel.state.value == LoadState.IDLE) {
+            viewModel.loadData()
+        }
+    }
+
+    private fun setMarkers(itemsList: List<BuildingItem>) {
+        // Create an instance of the Annotation API and get the PointAnnotationManager.
+        val annotationApi = mapView.annotations
+        val pointAnnotationManager = annotationApi.createPointAnnotationManager()
+        for (item in itemsList) {
+            // Set options for the resulting symbol layer.
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+                // Define a geographic coordinate.
+                .withPoint(Point.fromLngLat(
+                    item.address?.longitude?.toDouble()!!,
+                    item.address?.latitude?.toDouble()!!))
+                // Specify the bitmap you assigned to the point annotation
+                // The bitmap will be added to map style automatically.
+                .withIconImage(ContextCompat.getDrawable(requireContext(), R.drawable.ic_marker_uchebnoye)?.toBitmap()!!)
+            // Add the resulting pointAnnotation to the map.
+            pointAnnotationManager.create(pointAnnotationOptions)
+        }
+
+        // ВРЕМЕННОЕ РЕШЕНИЕ (ПОКА СЕРВАК НЕ ПОДНИМУТ) - РАССТАНОВКА ОДНОЙ ИКОНКИ ВРУЧНУЮ
+        // Set options for the resulting symbol layer.
+        val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+            // Define a geographic coordinate.
+            .withPoint(Point.fromLngLat(27.547,53.928))
+            // Specify the bitmap you assigned to the point annotation
+            // The bitmap will be added to map style automatically.
+            .withIconImage(ContextCompat.getDrawable(requireContext(), R.drawable.ic_marker_uchebnoye)?.toBitmap()!!)
+        // Add the resulting pointAnnotation to the map.
+        pointAnnotationManager.create(pointAnnotationOptions)
+        // ВРЕМЕННОЕ РЕШЕНИЕ (ПОКА СЕРВАК НЕ ПОДНИМУТ) - РАССТАНОВКА ОДНОЙ ИКОНКИ ВРУЧНУЮ
     }
 
     private fun onMapReady() {
@@ -146,6 +244,33 @@ class MapFragment : Fragment(), KoinComponent {
         mapView.location
             .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
         mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
+
+    private fun createSnackbar(message: String, color: Int) {
+        var timeLength = BaseTransientBottomBar.LENGTH_LONG
+        if (message == getString(R.string.loading))
+            timeLength = BaseTransientBottomBar.LENGTH_INDEFINITE
+        val snackbar = Snackbar.make(
+            binding.mapContainer,
+            message,
+            timeLength
+        )
+        snackbar.setTextColor(color)
+        snackbar.show()
+    }
+
+    private fun createSnackbarWithReload(snackbarTimeLength: Int, messageError: String) {
+        val snackbar = Snackbar.make(
+            binding.mapContainer,
+            messageError,
+            snackbarTimeLength
+        )
+        snackbar.setTextColor(requireContext().getColor(R.color.black))
+        snackbar.setActionTextColor(requireContext().getColor(R.color.black))
+        snackbar.setAction(R.string.reload) {
+            viewModel.loadData()
+        }
+        snackbar.show()
     }
 
     override fun onRequestPermissionsResult(
